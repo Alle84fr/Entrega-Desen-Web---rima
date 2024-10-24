8b4e486941c99ebc6d4f90db34191eee793c2a32
Rafaelawr <rafaelawr@hotmail.com>
2024-10-24 12:23:00 -0300

    Update listaEndereco.js

--- JS/listaEndereco.js
+++ JS/listaEndereco.js
@@ -1,18 +1,17 @@
+
 async function listarEndereco() {
 
     try{
 
-    api = await 
+    const responde = await 
     fetch("https://go-wash-api.onrender.com/api/auth/address")
     method:"GET",
     headers: {
         "Authorization"; " Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vYXBpLWdvLXdhc23Lmhlcm9rdWFwcC5jb20vYXBpL2xvZ2luIiwiaWF0IjoxNzEwNDE3MjIyLCJuYmYiOjE3MTA0MTcyMjIsImp0aSI6InBsZll0aENEZ0U1NUNzMHEiLCJzdWIiOiIxIiwicHJ2IjoiMjNiZDVjODk0OWY2MDBhZGIzOWU3MDFjNDAwODcyZGI3YTU5NzZmNyJ9.z1pdEBkx8Hq01B7jNKa42NGxtFFHwb-7O_0y8krVWUY' 
 
-
-
     }
 })
-    resposta = await api.json()
+    const resposta = await api.json()
     
    preencherTabela(data);
 } catch (error) {
@@ -20,11 +19,31 @@ async function listarEndereco() {
 }
 }
   function preencherTabela(endereco)
-  
-      
-    
-    resposta.data.forEach() =>{
-        console.log(endereco)
-    })
+  {
+    const tabela= document.querySelector();
+
+    tabela.innerHTML = "";
+
+    enderecos.forEach(endereco => {
+        const row = document.createElement("tr");
+
+        const ruaCell = document.createElement("td");
+        ruaCell.textContent = endereco.rua;
+
+        const cidadeCell = document.createElement("td");
+        cidadeCell.textContent = endereco.cidade;
+
+        const cidadeCell = document.createElement("td");
+        cidadeCell.textContent = endereco.cidade;
+
+        row.appendChild(ruaCell);
+        row.appendChild(cidadeCell);
+        row.appendChild(estadoCell);
+
+        tabela.appendChild(row);
+    });
 }
 
+fetchEndereco();
+
+  
