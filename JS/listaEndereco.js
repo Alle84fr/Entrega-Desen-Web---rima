8379f723da482f02b18165b3f74ddad87d6a87e0
Gortin <82157645+gortin1@users.noreply.github.com>
2024-10-24 17:04:46 -0300

    Update listaEndereco.js

--- JS/listaEndereco.js
+++ JS/listaEndereco.js
@@ -7,7 +7,7 @@ async function listarEndereco() {
     fetch("https://go-wash-api.onrender.com/api/auth/address")
     method:"GET",
     headers: {
-        "Authorization"; " Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vYXBpLWdvLXdhc23Lmhlcm9rdWFwcC5jb20vYXBpL2xvZ2luIiwiaWF0IjoxNzEwNDE3MjIyLCJuYmYiOjE3MTA0MTcyMjIsImp0aSI6InBsZll0aENEZ0U1NUNzMHEiLCJzdWIiOiIxIiwicHJ2IjoiMjNiZDVjODk0OWY2MDBhZGIzOWU3MDFjNDAwODcyZGI3YTU5NzZmNyJ9.z1pdEBkx8Hq01B7jNKa42NGxtFFHwb-7O_0y8krVWUY' 
+        "Authorization"; " Bearer $acessToken' 
 
     }
 })
